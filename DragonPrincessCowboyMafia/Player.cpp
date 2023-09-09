e0659f3d23942cb07ecb55b128f865fd1539e679
s3izures <seizuresthree@gmail.com>
2023-09-08 19:33:55 -0700

    Updated Player.cpp, not fully complete

--- DragonPrincessCowboyMafia/Player.cpp
+++ DragonPrincessCowboyMafia/Player.cpp
@@ -1,12 +1,18 @@
 #include "Player.h"
+#include "Items.h"
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
 
+//Constructors
 Player::Player()
 {
 	//Name and class
 	pName = "Felicia";
-	pClass = "Adventurer";
+	pClass = "None";
 
-	//Stats set, set to 0 as placeholder for now
+	//Stats set to default value, set to 0 as placeholder for now
 	pHp = 0;
 	pAtk = 0;
 	pDef = 0;
@@ -17,4 +23,189 @@ Player::Player()
 	pWeapon.atk = 0;
 	pShield.name = "None";
 	pShield.def = 0;
+}
+Player::Player(string pName, string pClass)
+{
+	//Name and class
+	this->pName = pName;
+	this->pClass = pClass;
+
+	//Stats set, set to 0 as placeholder for now
+	if (pClass == "Adventurer")
+	{
+		pHp = 0;
+		pAtk = 0;
+		pDef = 0;
+		pSpd = 0;
+	}
+	else if (pClass == "Warrior")
+	{
+		pHp = 0;
+		pAtk = 0;
+		pDef = 0;
+		pSpd = 0;
+	}
+	else if (pClass == "Pacifist")
+	{
+		pHp = 0;
+		pAtk = 0;
+		pDef = 0;
+		pSpd = 0;
+	}
+	else //use default stats from "Felicia"
+	{
+		pHp = 0;
+		pAtk = 0;
+		pDef = 0;
+		pSpd = 0;
+	}
+
+	//Placeholder stats atm
+	pWeapon.name = "Dull Dagger";
+	pWeapon.atk = 0;
+	pShield.name = "None";
+	pShield.def = 0;
+}
+
+//Getters
+string Player::getName()
+{
+	return pName;
+}
+string Player::getClass()
+{
+	return pClass;
+}
+int Player::getHp()
+{
+	return pHp;
+}
+int Player::getAtk()
+{
+	return pAtk;
+}
+int Player::getDef()
+{
+	return pDef;
+}
+int Player::getSpd()
+{
+	return pSpd;
+}
+weapon Player::getWeapon()
+{
+	return pWeapon;
+}
+shield Player::getShield()
+{
+	return pShield;
+}
+Items Player::getItem(int index)
+{
+	return pInv[index];
+}
+
+//Prints
+void Player::showProfile()
+{
+	cout << "---[ PROFILE ]---" << endl;
+	cout << "Princess " << getName() << ", the " << getClass() << endl; //name and class
+
+	cout << endl; //spacing
+
+	cout << "-- stats --" << endl; //stats
+	cout << "Health: " << getHp() << endl;
+	cout << "Attack: " << getAtk() << endl;
+	cout << "Defense: " << getDef() << endl;
+	cout << "Speed: " << getSpd() << endl;
+
+	cout << endl; //spacing
+
+	cout << "-- current;y equipped --" << endl; //weapon and shield
+	cout << "Weapon: " << pWeapon.name << " [+" << pWeapon.atk << " atk]" << endl;
+	cout << "Shield: " << pShield.name << " [+" << pShield.def << " def]" << endl;
+}
+void Player::showInventoryAll()
+{
+	for (int i = 0; i < pInv.size(); i++)
+	{
+		//get item name and print in list like [0] item
+	}
+}
+void Player::showItem()
+{
+	//Plan: have item class have getName and getDescription functions to print out
+	//Basically this function will go [ItemIndex] Item name - Item description
+}
+
+//Setters
+void Player::setHp(int val)
+{
+	pHp = val;
+}
+void Player::setAtk(int val)
+{
+	pAtk = val;
+}
+void Player::setDef(int val)
+{
+	pDef = val;
+}
+void Player::setSpd(int val)
+{
+	pSpd = val;
+}
+void Player::setWeapon(weapon newWeapon)
+{
+	pWeapon = newWeapon;
+}
+void Player::setShield(shield newShield)
+{
+	pShield = newShield;
+}
+void Player::addItem(Items&& newItem)
+{
+	//change 3 to max inventory slots if needed
+	if (pInv.size() < 3)
+	{
+		pInv.push_back(newItem);
+	}
+	else
+	{
+		cout << "Inventory Full!" << endl;
+		cout << "Discard an item from inventory? (yes/no): ";
+		string temp;
+		cin >> temp;
+		if (temp == "yes" || temp == "Yes" || temp == "YES")
+		{
+			bool done = false;
+			do
+			{
+				showInventoryAll();
+				cout << "Type in the index of the item you wish to remove: ";
+				int index = -1;
+				cin >> index;
+				if (index < pInv.size() || index >= 0)
+				{
+					removeItem(index);
+					done = true;
+				}
+				else
+				{
+					cout << "Invalid index, please try again." << endl;
+				}
+			} while (!done);
+
+			//Put new item in
+			pInv.push_back(newItem);
+		}
+		else if (temp == "no" || temp == "No" || temp == "NO")
+		{
+			cout << "Item dropped." << endl;
+		}
+		else
+		{
+			cout << "Invalid input, please try again!" << endl;
+		}
+	}
 }
--- DragonPrincessCowboyMafia/Player.h
+++ DragonPrincessCowboyMafia/Player.h
@@ -4,7 +4,7 @@
 #include <vector>
 using namespace std;
 
-//Weapon and Shield
+//Weapon and Shield, WILL BE CHANGED INTO CLASS FOR CONVENIENCE
 struct weapon //Player weapon
 {
 	string name;
@@ -22,8 +22,8 @@ class Player
 private:
 	string pName; //Player name, entered by player
 	string pClass; //Selected by player: Adventurous, Warrior, and Pacifist (will affect stats)
-	weapon pWeapon;
-	shield pShield;
+	weapon pWeapon; //Gives player bonus atk
+	shield pShield; //Gives player bonus def
 
 	//Player stats, determined by class and may or may not change according to equipment
 	int pHp;
@@ -39,18 +39,26 @@ public:
 	Player(string pName, string pClass);
 
 	//Getters
-	string getName();
-	string getClass();
-	string getWeapon();
-	string getShield();
+	string getName(); //Will be used for dialogue (EX: This is princess namehere)
+	string getClass(); //Will be used for dialogue, or maybe variation (EX: warrior class gets different encounter/dialogue variation, etc)
+	int getHp();
+	int getAtk();
+	int getDef();
+	int getSpd();
+	weapon getWeapon();
+	shield getShield();
 	Items getItem(int index); //To print all, for loop the entire vector
 
 	//Prints
 	void showProfile();
-	void showInventoryAll();
-	void showItem();
+	void showInventoryAll(); //WIP!!!
+	void showItem(); //WIP!!!
 
-	//Setter / Add
+	//Setter & the like
+	void setHp(int val);
+	void setAtk(int val);
+	void setDef(int val);
+	void setSpd(int val);
 	void setWeapon(weapon newWeapon);
 	void setShield(shield newShield);
 	void addItem(Items&& newItem); //If player has too many items, maybe ask which to remove?
