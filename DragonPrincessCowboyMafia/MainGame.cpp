5313065aea857da52f63df1ca456b6f9675b8431
s3izures <seizuresthree@gmail.com>
2023-09-08 20:51:43 -0700

    Initialization of game added

--- DragonPrincessCowboyMafia/MainGame.cpp
+++ DragonPrincessCowboyMafia/MainGame.cpp
@@ -1,24 +1,35 @@
 #include "Player.h"
 #include "Items.h"
 #include <iostream>
+#include <time.h>
 using namespace std;
 
-//Functions
-void startScreen();
+//Utility Functions
+void startScreen(); //WIP
 Player characterCreation();
+int diceRoll(int sides);
 
 
 
 //Main
 int main()
 {
-	//will change dialogue to be more immersive
-	startScreen();
+	//GAME INITIALIZATION
+		//Randomly generated number that will determine which random events appear in a single session
+		int chaosControl = diceRoll(3);
 
-	//Player creation
-	Player player = characterCreation();
+		//WIP: will change dialogue to be more immersive
+		startScreen();
+		system("CLS");
 
-	player.showProfile();
+		//Player creation
+		Player player = characterCreation();
+		system("CLS");
+
+
+
+	//GAME STORY
+		//wip one sec
 }
 
 
@@ -27,66 +38,127 @@ void startScreen()
 {
 	cout << "WIP";
 
+	//Add option to start game
 }
-
 Player characterCreation()
 {
 	//Values needed to create the o class
 	string pName;
 	string pClass;
 
-	//Name
-	cout << "Enter name: ";
-	cin >> pName;
+	//Name: loop until player confirms
+	bool nameDone = false;
 
-	cout << endl; //spacing
+		do
+		{
+			cout << "Enter name: ";
+			cin >> pName;
+
+			bool confirm = false;
+			do
+			{
+				cout << "Is this your name? (yes/no): ";
+				string temp;
+				cin >> temp;
+
+				if (temp == "yes" || temp == "Yes" || temp == "YES")
+				{
+					nameDone = true;
+					confirm = true;
+				}
+				else if (temp == "no" || temp == "No" || temp == "NO")
+				{
+					nameDone = false;
+					confirm = true;
+				}
+				else
+				{
+					cout << "Invalid input, please try again!" << endl;
+				}
+			} while (!confirm);
+
+		} while (!nameDone);
+
+		system("CLS"); //Clear screen
 
 	//Class Selection:
 	//done via entering number to avoid spelling error mistakes from player
 	bool classDone = false;
 
-	//Display paths
-	cout << "Select a path: " << endl;
-	cout << endl;
-	cout << "[1] Adventurer" << endl;
-	cout << "DESCRIPTION HERE" << endl;
-	cout << endl;
-	cout << "[2] Warrior" << endl;
-	cout << "DESCRIPTION HERE" << endl;
-	cout << endl;
-	cout << "[3] Pacifist" << endl;
-	cout << "DESCRIPTION HERE" << endl;
-	cout << endl;
+		//Display paths
+		cout << "Select a path: " << endl;
+		cout << endl;
+		cout << "[1] Adventurer" << endl;
+		cout << "DESCRIPTION HERE" << endl;
+		cout << endl;
+		cout << "[2] Warrior" << endl;
+		cout << "DESCRIPTION HERE" << endl;
+		cout << endl;
+		cout << "[3] Pacifist" << endl;
+		cout << "DESCRIPTION HERE" << endl;
+		cout << endl;
 
-	do
-	{
-		cout << "Enter the number/index of your chosen class: ";
-		int tempClass = 0;
-		cin >> tempClass;
+		do //Will loop until player gets class right
+		{
+			cout << "Enter the number/index of your chosen class: ";
+			int tempClass = 0;
+			cin >> tempClass;
 
-		if (tempClass == 1)
-		{
-			pClass = "Adventurer";
-			classDone = true;
-		}
-		else if (tempClass == 2)
-		{
-			pClass = "Warrior";
-			classDone = true;
-		}
-		else if (tempClass == 3)
-		{
-			pClass = "Pacifist";
-			classDone = true;
-		}
-		else
-		{
-			tempClass = 0;
-			classDone = false;
-			cout << "Invalid input, please try again." << endl;
-		}
-	} while (!classDone);
+			if (tempClass == 1)
+			{
+				pClass = "Adventurer";
+				classDone = true;
+			}
+			else if (tempClass == 2)
+			{
+				pClass = "Warrior";
+				classDone = true;
+			}
+			else if (tempClass == 3)
+			{
+				pClass = "Pacifist";
+				classDone = true;
+			}
+			else
+			{
+				tempClass = 0;
+				classDone = false;
+				cout << "Invalid input, please try again." << endl;
+			}
+
+			if (classDone) //confirm
+			{
+				bool confirm = false;
+				do
+				{
+					cout << "Your chosen path is " << pClass << ". Confirm? (yes/no): ";
+					string temp;
+					cin >> temp;
+					if (temp == "yes" || temp == "Yes" || temp == "YES")
+					{
+						classDone = true;
+						confirm = true;
+					}
+					else if (temp == "no" || temp == "No" || temp == "NO")
+					{
+						classDone = false;
+						confirm = true;
+					}
+					else
+					{
+						cout << "Invalid input, please try again!" << endl;
+					}
+				} while (!confirm);
+			}
+
+		} while (!classDone);
 
 	Player temp(pName, pClass);
 	return temp;
+}
+int diceRoll(int sides)
+{
+	//Sides stands for the number of sides on a dice
+	srand(time(NULL));
+	return rand() % sides + 1;
 }
