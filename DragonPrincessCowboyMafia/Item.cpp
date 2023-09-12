ef6704ce3a06b4b8e577c5f24d746f4c34d6841d
btOfclAcc <brycettc@gmail.com>
2023-09-12 07:17:33 -0700

    Added Item member functions

--- DragonPrincessCowboyMafia/Item.cpp
+++ DragonPrincessCowboyMafia/Item.cpp
@@ -1,1 +1,52 @@
 #include "Item.h"
+
+Item::Item(string t_name, string t_effect, int t_effectValue, int t_uses, string t_description, string t_useMessage)
+{
+	name = t_name;
+	effect = t_effect;
+	effectValue = t_effectValue;
+	uses = t_uses;
+	description = t_description;
+	useMessage = t_useMessage;
+}
+
+Item::~Item()
+{
+}
+
+const string Item::GetName()
+{
+	return name;
+}
+
+const string Item::GetEffect()
+{
+	return effect;
+}
+
+const int Item::GetEffectValue()
+{
+	return effectValue;
+}
+
+const int Item::GetUses()
+{
+	return uses;
+}
+
+const string Item::GetDescription()
+{
+	return description;
+}
+
+const string Item::GetUseMessage()
+{
+	return useMessage;
+}
+
+bool Item::operator==(const Item& t_item)
+{
+	if (name == t_item.name)
+		return true;
+	return false;
+}
--- DragonPrincessCowboyMafia/Item.h
+++ DragonPrincessCowboyMafia/Item.h
@@ -1,14 +1,27 @@
+#include <string>
 #pragma once
+using namespace std;
 class Item
 {
 private:
 	string name;
 	string effect;
 	int effectValue;
+	int uses;
 	string description;
 	string useMessage;
 
 public:
+	Item(string, string, int, int, string, string);
+	~Item();
 
+	const string GetName();
+	const string GetEffect();
+	const int GetEffectValue();
+	const int GetUses();
+	const string GetDescription();
+	const string GetUseMessage();
+
+	bool operator == (const Item&);
 };
 
