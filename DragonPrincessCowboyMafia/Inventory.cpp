aea7da0dd7b79e2f1400a2154313bb684ce4ba13
btOfclAcc <brycettc@gmail.com>
2023-09-15 11:26:32 -0700

    Update Inventory.cpp

--- DragonPrincessCowboyMafia/Inventory.cpp
+++ DragonPrincessCowboyMafia/Inventory.cpp
@@ -58,6 +58,7 @@ void Inventory::AddItem(Item t_item)
     if (!itemFound) {
         inventory.push_back(invenItem{ t_item , t_item.GetUses(), 1});
     }
+    cout << "You obtained " << t_item.GetName() << " x1" << endl;
 }
 
 void Inventory::UseItem(Item t_item)
@@ -65,6 +66,7 @@ void Inventory::UseItem(Item t_item)
     for (auto t_invenItem : inventory) {
         if (t_invenItem.item == t_item) {
             t_invenItem.uses -= 1;
+            cout << t_invenItem.item.GetUseMessage() << endl;
             if (t_invenItem.uses == 0) {
                 t_invenItem.count -= 1;
                 if (t_invenItem.count == 0) {
